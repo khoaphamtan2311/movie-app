22c31b9ce121e9710d06ee458e2f72adbaeaae2b
khoaphamtan2311 <tankhoapham2311@gmail.com>
2023-07-01 14:54:11 +0700

    first commit

---
+++ src/layouts/MainFooter.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+function MainFooter() {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      p={1}
+      mt={2}
+    ></Typography>
+  );
+}
+
+export default MainFooter;
